a193ae779626d8d40d7223a8821ba3fd479e9d11
ekschultz777 <tedkschultz@gmail.com>
2025-04-12 03:23:36 -0700

    updated list item view

--- TeeFinder/ViewComponents/CourseListView.swift
+++ TeeFinder/ViewComponents/CourseListView.swift
@@ -17,32 +17,71 @@ struct CourseListView: View {
     var body: some View {
         List {
             ForEach(viewModel.items) { item in
-                NavigationLink(item.clubName) {
-                    VStack {
-                        VStack {
-                            Text(item.clubName)
-                                .foregroundStyle(Color.white)
-                                .padding()
-                            Text(item.courseName)
-                                .foregroundStyle(Color.white)
-                                .padding()
-                            Text(item.location.address)
-                                .foregroundStyle(Color.white)
-                                .padding()
-                        }
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background {
-                        Color.black.opacity(0.9)
-                            .ignoresSafeArea()
-                    }
-                    .onAppear {
-                        // If we check out a course we should persist it in the future.
-                        viewModel.persist(item)
-                    }
-                }
+                CourseListItemView(viewModel: viewModel, 
+                                   course: item,
+                                   clubName: item.clubName,
+                                   location: item.location.address,
+                                   rating: 71)
             }
         }
         .scrollContentBackground(.hidden)
+        .listRowSeparator(.hidden)
+        .listRowSpacing(8)
+    }
+}
+
+struct CourseListItemView: View {
+    typealias CourseListItemViewModel = CourseListViewModel
+    let viewModel: CourseListItemViewModel
+    let course: Course
+    let clubName: String
+    let location: String
+    let rating: Double
+    var body: some View {
+        NavigationLink(destination: {
+            GolfCourseDetailView(courseName: clubName)
+                .onAppear {
+                    // If we check out a course we should persist it in the future.
+                    viewModel.persist(course)
+                }
+        }, label: {
+            HStack(alignment: .top, spacing: 8) {
+                // Optional image or icon
+                Image(systemName: "flag.circle.fill")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(.black.opacity(0.9))
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(clubName)
+                        .font(.headline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.primary)
+                    HStack(spacing: 4) {
+                        Image(systemName: "mappin.and.ellipse")
+                        Text(location)
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    HStack(spacing: 4) {
+                        Text(String(format: "%.1f", rating))
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+        })
+    }
+}
+
+
+struct GolfCourseDetailView: View {
+    var courseName: String
+
+    var body: some View {
+        Text("Welcome to \(courseName)")
+            .font(.largeTitle)
+            .padding()
     }
 }
